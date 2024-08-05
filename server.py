4c13179a4e761c4dc86b0a4a773f8ac29dd02858
Dan Moore <dan@fusionauth.io>
2024-08-05 10:22:42 -0600

    support for login from laptop to phone and vice versa

--- server.py
+++ server.py
@@ -6,6 +6,13 @@ from urllib.parse import quote_plus, urlencode
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, redirect, render_template, session, url_for, request, make_response
+import qrcode
+import qrcode.image.svg
+import requests
+import threading
+import time
+from fusionauth.fusionauth_client import FusionAuthClient
+
 
 ACCESS_TOKEN_COOKIE_NAME = "cb_access_token"
 REFRESH_TOKEN_COOKIE_NAME = "cb_refresh_token"
@@ -31,12 +38,41 @@ oauth.register(
   server_metadata_url=f'{env.get("ISSUER")}/.well-known/openid-configuration'
 )
 
+client = FusionAuthClient(env.get("API_KEY"), env.get("ISSUER"))
+
+polling_data = {'content': '', 'url': 'https://api.example.com/data', 'interval': 10}
+polling_lock = threading.Lock()
+
+def poll_url():
+    while True:
+        with polling_lock:
+            interval = polling_data['interval']
+        try:
+            token_url = env.get("ISSUER")
+            data = {
+              "client_id": env.get("CLIENT_ID"),
+              "grant_type": "urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code"
+ 
+            }
+
+            response = requests.get('http://localhost:9011/oauth2/token')
+            if response.status_code == 200:
+              print("request received")
+              polling_data['content'] = response.text
+        except Exception as e:
+            polling_data['content'] = f"Error: {e}"
+        time.sleep(interval)
+
+
 if __name__ == "__main__":
+  polling_thread = threading.Thread(target=poll_url, daemon=True)
+  polling_thread.start()
   app.run(host="0.0.0.0", port=env.get("PORT", 5000))
 
 def get_logout_url():
   return env.get("ISSUER") + "/oauth2/logout?" + urlencode({"client_id": env.get("CLIENT_ID")},quote_via=quote_plus)
 #end::baseApplication[]
+
 
 #tag::homeRoute[]
 @app.route("/")
@@ -46,6 +82,15 @@ def home():
     return redirect("/account")
 
   return render_template("home.html")
+#end::homeRoute[]
+
+@app.route("/device_grant_data")
+def device_grant_data():
+    content = "n/a"
+    with polling_lock:
+        content = polling_data['content']
+    print(content)
+    return ""
 #end::homeRoute[]
 
 
@@ -105,6 +150,60 @@ def account():
     session=json.loads(request.cookies.get(USERINFO_COOKIE_NAME, None)),
     logoutUrl=get_logout_url())
 #end::accountRoute[]
+
+#
+# This is the page displayed when you are logged out on your laptop but want to login with a QR code read by the phone
+#
+@app.route("/logged-out-qr-login")
+def logged_out_qr_login():
+  qr = qrcode.QRCode(image_factory=qrcode.image.svg.SvgPathImage)
+
+  device_start_url = env.get("ISSUER") + '/oauth2/device_authorize'
+  data = { "client_id": env.get("CLIENT_ID") }
+  r = requests.post(device_start_url,headers={},data=data)
+  verification_url_complete = r.json()['verification_uri_complete']
+  qr.add_data(verification_url_complete)
+  qr.make(fit=True)
+  qrimg = qr.make_image(attrib={'class': 'some-css-class'}).to_string(encoding='unicode')
+  return render_template(
+    "logged-out-qr-login.html",
+    qrimg=qrimg)
+
+#
+# This is the page displayed when you are logged in on your laptop but want to login using a QR code on your phone
+#
+@app.route("/logged-in-qr-login")
+def logged_in_qr_login():
+  access_token = request.cookies.get(ACCESS_TOKEN_COOKIE_NAME, None)
+  refresh_token = request.cookies.get(REFRESH_TOKEN_COOKIE_NAME, None)
+
+  user = session["user"]
+
+  if access_token is None:
+    return redirect(get_logout_url())
+
+  passwordless_request = {
+    'loginId': user["email"],
+    'applicationId': env.get("CLIENT_ID")
+  }
+
+  response = client.start_passwordless_login(passwordless_request)
+  if response.error_response:
+    print("in error")
+    print(response.error_response)
+  response_json = response.success_response
+  code = response_json["code"]
+
+  qr = qrcode.QRCode(image_factory=qrcode.image.svg.SvgPathImage)
+
+  login_start_url = env.get("ISSUER") + '/oauth2/passwordless/' +code+ ' ?postMethod=true'
+
+  qr.add_data(login_start_url)
+  qr.make(fit=True)
+  qrimg = qr.make_image(attrib={'class': 'some-css-class'}).to_string(encoding='unicode')
+  return render_template(
+    "logged-in-qr-login.html",
+    qrimg=qrimg)
 
 
 #
